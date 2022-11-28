3f68fad4a5f83898784cfa5c68318b006341ee28
samethozman <samet.hozman@outlook.com>
2022-11-28 14:11:49 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+let superbowlWin = function (arrayOfObjects) {
+    let winningYear = arrayOfObjects.find((game) => (
+    game.result === "W"
+    ))
+    return winningYear ? winningYear.year : winningYear;
+}
